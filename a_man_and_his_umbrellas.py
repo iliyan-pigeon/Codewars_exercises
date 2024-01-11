586038e6c230f76c1d8437add73a4e1df9d48e81
Iliyan Galabov <milenov556@gmail.com>
2024-01-11 12:23:49 +0200

    Exercise updated and completed. All tests passed.

--- a_man_and_his_umbrellas.py
+++ a_man_and_his_umbrellas.py
@@ -1,13 +1,22 @@
 def min_umbrellas(weather):
     umbrellas_needed = 0
+    umbrellas_home = 0
+    umbrellas_at_work = 0
 
     for i in range(len(weather)):
         if i % 2 == 0:
             if weather[i] == 'rainy' or weather[i] == 'thunderstorms':
-                umbrellas_needed += 1
+                if umbrellas_home > 0:
+                    umbrellas_home -= 1
+                umbrellas_at_work += 1
         else:
             if weather[i] == 'rainy' or weather[i] == 'thunderstorms':
-                if not weather[i-1] == 'rainy' or not weather[i-1] == 'thunderstorms':
-                    umbrellas_needed += 1
+                if umbrellas_at_work > 0:
+                    umbrellas_at_work -= 1
+                umbrellas_home += 1
 
+    umbrellas_needed = umbrellas_home + umbrellas_at_work
     return umbrellas_needed
+
+
+print(min_umbrellas(["rainy", "rainy", "rainy", "rainy"]))
