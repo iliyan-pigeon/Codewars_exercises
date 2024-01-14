b82b7f2422ae70476db7bf1659d15571a411892d
Iliyan Galabov <milenov556@gmail.com>
2024-01-14 12:22:01 +0200

    Update hill_cipher_encryption.py

--- hill_cipher_encryption.py
+++ hill_cipher_encryption.py
@@ -1,7 +1,13 @@
 def encrypt(text, key):
-    the_text = [i for i in text.strip(" ") if i.isalpha()]
+    alphabet_dict = {'a': 1, 'b': 2, 'c': 3, 'd': 4, 'e': 5, 'f': 6, 'g': 7,
+                     'h': 8, 'i': 9, 'j': 10, 'k': 11, 'l': 12, 'm': 13, 'n': 14,
+                     'o': 15, 'p': 16, 'q': 17, 'r': 18, 's': 19, 't': 20, 'u': 21,
+                     'v': 22, 'w': 23, 'x': 24, 'y': 25, 'z': 26}
+    the_text = [alphabet_dict[i.lower()] for i in text.strip(" ") if i.isalpha()]
     text_matrix = []
-    key_matrix = [[key[0], key[1]], [key[2], key[3]]]
+    key_matrix = [[alphabet_dict[key[0]], alphabet_dict[key[1]]], [alphabet_dict[key[2]], alphabet_dict[key[3]]]]
+    print(the_text)
+    print(key_matrix)
 
 
 encrypt('Five + Seven = Twelve', 'math')
