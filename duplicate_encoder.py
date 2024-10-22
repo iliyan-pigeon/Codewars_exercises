758848df047138435e7dbf007657ecd6a87e26a6
Iliyan Galabov <milenov556@gmail.com>
2024-10-22 09:40:03 +0300

    Create duplicate_encoder.py

---
+++ duplicate_encoder.py
@@ -0,0 +1,12 @@
+def duplicate_encode(word):
+
+    word = word.upper()
+    result = ""
+    for char in word:
+        if word.count(char) > 1:
+            result += ")"
+        else:
+            result += "("
+            
+    return result
+  
